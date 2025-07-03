aacea41dd6b6fd7cd0d66664036f27baef031952
premyadav-48 <premyadav4824@gmail.com>
2025-07-03 16:18:27 +0545

    Just Beforre Page Routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import MovieCard from "./components/MovieCard";
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+
+
+  return (
+    <>
+      <Home></Home>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    { id: 1, title: "John wick", release_date: "2020" },
+    { id: 2, title: "Spiderman 3", release_date: "2021" },
+    { id: 3, title: "Superman", release_date: "2022" },
+  ];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("......");
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for a movie..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movies.map(
+          (movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery) && (
+              <MovieCard movie={movie} key={movie.id} />
+            )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
